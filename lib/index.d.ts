882641183a5e7824d3277e389129121f60d08ec5
lichun <lichun@mistong.com>
2020-08-14 13:54:35 +0800

    optimate

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -7,6 +7,6 @@ export declare const Controller: typeof Resource;
 export declare const resetModel: <T>(T: new () => T) => void;
 export declare function AutoWired<T>(T: {
     new (): T;
-} | String): (clazz: any, attr: any) => void;
+} | String): (clazz: any, attr: T) => void;
 declare const _default: (store: any, asyncReducers?: {}) => void;
 export default _default;
