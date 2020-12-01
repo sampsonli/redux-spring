4c8a10b2a3087161affc70dbd2b1d966863d0cc2
lichun <lichun@mistong.com>
2020-12-01 16:03:39 +0800

    optimate

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,18 +1,26 @@
 import spring, {Model, service, useModel} from '../src'
 import {createStore} from 'redux';
 
-const store = createStore(() => {});
-spring(store);
-
-@service('test')
-class Test extends Model {
-    num = 0;
-    add(count) {
-        this.num = this.num + count;
-    }
-}
 
 test('test class function', () => {
+    const store = createStore(() => {});
+    spring(store);
+    @service('test')
+    class Test extends Model {
+        num = 0;
+        result = 0;
+        add(count) {
+            this.num = this.num + count;
+        }
+        * getFromRemote() {
+            this.result = yield new Promise((resolve) => {
+                setTimeout(() => {
+                    resolve(12);
+                }, 1000);
+            });
+            return this.result;
+        }
+    }
     let model = store.getState().test;
 
     expect(model.num).toBe(0);
@@ -23,4 +31,3 @@ test('test class function', () => {
     model = store.getState().test;
     expect(model.num).toBe(20);
 })
-
