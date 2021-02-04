bbb7584fd9c9d19d95bfae9cf58b2fa8bd2d1cdc
lichun <lichun@mistong.com>
2021-02-04 17:58:32 +0800

    fixbug

--- src/util.ts
+++ src/util.ts
@@ -11,12 +11,10 @@ export function assign(target, from) {
 export function isGenerator(fn: Function): boolean {
     // ts 配置es5 有个bug, 只能按照下面的方式解决了
     if(process.env.NODE_ENV !== 'test') { // 浏览器环境
-        const str = Object.prototype.toString.call(fn);
-        if(str === '[object Function]' && fn.prototype && fn.prototype.toString() === '[object Generator]') {  // 可能是ie
+        if(fn.prototype && fn.prototype.toString() === '[object Generator]') {  // 可能是ie
             return true;
         }
-        // [object GeneratorFunction] or [object Generator]
-        return str === '[object GeneratorFunction]'
+        return Object.prototype.toString.call(fn) === '[object GeneratorFunction]'
     }
     return fn.toString().indexOf('generator') > -1;
 
